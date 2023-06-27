45ede7136b62ebff147f346fbe56f7c8718950a6
Henrik Zimmermann <henrik@zi-data.com>
2023-06-27 15:46:07 -0400

    Moved all client / server events code into one file

--- events.py
+++ events.py
@@ -1,24 +1,48 @@
+"""
+Implementation of a simple event-based network protocol.
+"""
+
 from __future__ import annotations
 from typing import Optional
 
 import selectors
 from selectors import DefaultSelector
 import socket
-from time import sleep
-
-from PySide6.QtCore import QObject, QRunnable, Signal, QThreadPool
 from queue import Queue
+import sys
 
+from PySide6.QtCore import QObject, QRunnable, Signal
+from PySide6.QtWidgets import QApplication, QWidget, QHBoxLayout, \
+    QVBoxLayout, QLineEdit, QPushButton, QLabel
+from PySide6.QtCore import QThreadPool
 
+# The port that is used by clients and servers.
 PORT = 3000
+
+# The timeout for blocking reads in the client and select in the server.
+TIMEOUT = 0.001
 
 
 class Event:
+    """
+    One event that is sent between the client and the server.
+    """
+    name: str
+    payload: Optional[list]
+
+
     def __init__(self, name: str, payload: Optional[list] = None) -> None:
+        """
+        Initialize a new event with the given name and payload.
+        """
         self.name = name
         self.payload = payload
 
     def toBytes(self) -> bytes:
+        """
+        Convert this event into a sequence of bytes that can be sent over the
+        network.
+        """
         if self.payload is None or len(self.payload) == 0:
             return f"{self.name}\n".encode()
         else:
@@ -26,6 +50,9 @@ class Event:
         
     @staticmethod
     def fromBytes(data: bytes) -> Event:
+        """
+        Create an event from the given sequence of bytes.
+        """
         data = data.decode()[:-1]
         if ":" in data:
             data = data.split(":")
@@ -36,40 +63,79 @@ class Event:
             return Event(data)
         
     def __str__(self) -> None:
+        """
+        Return a string representation of this event.
+        """
         return f"{self.name}:{self.payload}"
+
         
-
-class ServerReceiver(QObject, QRunnable):
+class Server(QObject, QRunnable):
+    """
+    The server that excepts events from all clients and broadcasts events
+    to all clients at once.
+    """
     eventReceived = Signal(Event)
+    conns: list[socket.socket]
+    msgQueue: Queue[Event]
 
-    def __init__(self, conns: list[socket.socket]) -> None:
+    def __init__(self) -> None:
         QObject.__init__(self)
         QRunnable.__init__(self)
+
+        self.conns = []
+
+        self.msgQueue = Queue()
+
         self.sel = DefaultSelector()
         self.sock = socket.create_server(("localhost", PORT))
         self.sock.listen(5)
-        self.sock.setblocking(False)
         self.sel.register(self.sock, selectors.EVENT_READ, self.accept)
-        self.conns = conns
-        self.shutdown = False
+        self.shouldClose = False
 
     def run(self) -> None:
-        while not self.shutdown:
-            events = self.sel.select(0.02)
+        """
+        Run the server until it is closed. Select for the client connections
+        and process the message queue repeatedly.
+        """
+        while not self.shouldClose:
+            events = self.sel.select(0.001)
             for key, mask in events:
                 callback = key.data
                 callback(key.fileobj, mask)
+                print("Read")
+
+            if not self.msgQueue.empty():
+                e = self.msgQueue.get()
+                data = e.toBytes()
+                for conn in self.conns:
+                    conn.send(data)
 
         self.sel.close()
+        
+        for conn in self.conns:
+            conn.close()
+        self.conns.clear()
+        self.sock.close()
+
+    def send(self, e: Event) -> None:
+        """
+        Put an event in the message queue to be broadcast to all clients.
+        """
+        self.msgQueue.put(e)
 
     def accept(self, sock: socket.socket, mask) -> None:
+        """
+        Accept a new connection and register it with the selector.
+        """
         conn, addr = sock.accept()
-        conn.setblocking(False)
         self.sel.register(conn, selectors.EVENT_READ, self.read)
         self.conns.append(conn)
 
     def read(self, sock: socket.socket, mask) -> None:
-        data = sock.recv(1000)
+        """
+        Read from a connection and emit the event that was received.
+        """
+        data = sock.recv(1024)
 
         if data:
             self.eventReceived.emit(Event.fromBytes(data))
@@ -79,61 +145,20 @@ class ServerReceiver(QObject, QRunnable):
             sock.close()
 
     def close(self) -> None:
-        self.shutdown = True
-
-class ServerSender(QObject, QRunnable):
-    def __init__(self, conns: list[socket.socket]) -> None:
-        QObject.__init__(self)
-        QRunnable.__init__(self)
-        self.conns = conns
-        self.shutdown = False
-
-    def run(self) -> None:
-        while not self.shutdown:
-            sleep(1)
-
-    def emit(self, event: Event) -> None:
-        data = event.toBytes()
-
-        for conn in self.conns:
-            conn.send(data)
-
-    def close(self) -> None:
-        self.shutdown = True
-
-        
-class Server(QObject, QRunnable):
-    eventReceived = Signal(Event)
-    conns: list[socket.socket]
-
-    def __init__(self) -> None:
-        QObject.__init__(self)
-        self.conns = []
-        self.serverReceiver = ServerReceiver(self.conns)
-        self.serverSender = ServerSender(self.conns)
-
-        self.serverReceiver.eventReceived.connect(self.eventReceived)
-
-        pool = QThreadPool.globalInstance()
-        pool.start(self.serverReceiver)
-        pool.start(self.serverSender)
-
-    def send(self, e: Event) -> None:
-        self.serverSender.emit(e)
-
-    def close(self) -> None:
-        for conn in self.conns:
-            conn.close()
-        self.conns.clear()
-
-        self.serverReceiver.close()
-        self.serverSender.close()
+        """
+        Initiate the shutdown of the server. Messages remaining in the queue
+        are dropped.
+        """
+        self.shouldClose = True
 
 
 class Client(QObject, QRunnable):
     eventReceived = Signal(Event)
 
     def __init__(self) -> None:
+        """
+        Initialize a new client. It connects to localhost:PORT.
+        """
         QObject.__init__(self)
         QRunnable.__init__(self)
 
@@ -141,10 +166,14 @@ class Client(QObject, QRunnable):
         self.conn = socket.create_connection(("localhost", PORT))
         self.conn.settimeout(0.001)
 
-        self.shutdown = False
+        self.shouldClose = False
 
     def run(self) -> None:
-        while not self.shutdown:
+        """
+        Run the client until it is closed. Receive events and send events
+        repeatedly.
+        """
+        while not self.shouldClose:
             timedOut = False
             try:
                 data = self.conn.recv(1024)
@@ -167,7 +196,55 @@ class Client(QObject, QRunnable):
         self.conn.close()
 
     def send(self, e: Event) -> None:
+        """
+        Enqueue a message to be sent to the server.
+        """
         self.msgQueue.put(e)
 
     def close(self) -> None:
-        self.shutdown = True
+        """
+        Initialize the shutdown of the client. Messages remaining in the queue
+        are dropped.
+        """
+        self.shouldClose = True
+
+
+if __name__ == "__main__":
+    """
+    A sample application that can act as server or client.
+    """
+
+    app = QApplication()
+    window = QWidget()
+
+    if len(sys.argv) > 1 and sys.argv[1] == "server":
+        endpoint = Server()
+        title = "Server"
+    else:
+        endpoint = Client()
+        title = "Client"
+    
+    QThreadPool.globalInstance().start(endpoint)
+
+    vBox = QVBoxLayout()
+    window.setLayout(vBox)
+
+    hBox = QHBoxLayout()
+    vBox.addLayout(hBox)
+
+    edit = QLineEdit()
+    hBox.addWidget(edit)
+
+    submit = QPushButton("Send")
+    submit.clicked.connect(lambda: endpoint.send(Event(edit.text())))
+    hBox.addWidget(submit)
+
+    label = QLabel()
+    endpoint.eventReceived.connect(lambda e: label.setText(e.name))
+    vBox.addWidget(label)
+
+    window.setWindowTitle(title)
+    window.show()
+    code = app.exec()
+    endpoint.close()
+    sys.exit(code)
