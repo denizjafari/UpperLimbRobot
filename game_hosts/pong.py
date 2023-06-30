ef6856b87863ab2df3fcde298e97e328f9148591
Henrik Zimmermann <henrik@zi-data.com>
2023-06-30 12:56:14 -0400

    Build basic pong game

---
+++ game_hosts/pong.py
@@ -0,0 +1,281 @@
+"""
+The pong game implemented using the Qt framework.
+
+Author: Henrik Zimmermann <henrik.zimmermann@utoronto.ca>
+"""
+
+from __future__ import annotations
+from typing import Optional
+
+import sys
+
+from PySide6.QtCore import QTimer, Qt
+from PySide6.QtWidgets import QWidget, QLabel, QApplication, QVBoxLayout, \
+    QPushButton
+from PySide6.QtGui import QPaintEvent, QPainter, QKeyEvent
+
+
+SQUARE_SIZE = 600
+DEFAULT_PADDLE_SIZE = 50
+
+NEUTRAL = 0
+LEFT = 1
+RIGHT = 2
+UP = 3
+DOWN = 4
+
+
+class Paddle:
+    """
+    One paddle that can be moved up and down by the player.
+    """
+    def __init__(self, side:int=LEFT) -> None:
+        """
+        Initialize the paddle.
+        """
+        self.size = DEFAULT_PADDLE_SIZE
+        self.position = SQUARE_SIZE // 2
+        self.thickness = 10
+        self.side = side
+        self.speed = 5
+        self.movingUp = False
+        self.movingDown = False
+
+    def topEdge(self) -> float:
+        """
+        Return the top y coordinate of the paddle.
+        """
+        return self.position - self.size // 2
+    
+    def bottomEdge(self) -> float:
+        """
+        Return the bottom y coordinate of the paddle.
+        """
+        return self.position + self.size // 2
+    
+    def isHit(self, ball: Ball) -> bool:
+        """
+        Determine whether the ball is hit by the paddle.
+        """
+        if self.side == LEFT:
+            inXRange = ball.leftEdge() <= self.thickness
+        else:
+            inXRange = ball.rightEdge() >= SQUARE_SIZE - self.thickness
+        
+        inYRange = self.bottomEdge() >= ball.centerY() >= self.topEdge()
+            
+        return inXRange and inYRange
+    
+    def move(self) -> None:
+        """
+        Move the paddle according to the moving up and down attributes.
+        """
+        if self.movingUp:
+            self.position -= self.speed
+        elif self.movingDown:
+            self.position += self.speed
+
+    def paint(self, painter: QPainter) -> None:
+        """
+        Paint the paddle to an active painter.
+        """
+        painter.fillRect(0 if self.side == LEFT else SQUARE_SIZE - self.thickness,
+                         self.position - self.size // 2,
+                         self.thickness,
+                         self.size,
+                         Qt.black)
+        
+
+class Ball:
+    """
+    The ball that bounds off the walls and paddles.
+    """
+    def __init__(self):
+        """
+        Initialize the ball.
+        """
+        self.radius = 10
+        self.position = SQUARE_SIZE // 2, SQUARE_SIZE // 2
+    
+    def leftEdge(self) -> float:
+        """
+        Return the left x coordinate of the ball.
+        """
+        return self.position[0] - self.radius
+    
+    def rightEdge(self) -> float:   
+        """
+        Return the right x coordinate of the ball.
+        """
+        return self.position[0] + self.radius
+    
+    def centerY(self) -> float:
+        """
+        Return the y coordinate at the center of the ball.
+        """
+        return self.position[1]
+    
+    def bottomEdge(self) -> float:
+        """
+        Return the y coordinate at the bottom of the ball.
+        """
+        return self.position[1] + self.radius
+    
+    def topEdge(self) -> float:
+        """
+        Return the y coordinate at the tio of the ball.
+        """
+        return self.position[1] - self.radius
+    
+    def move(self, x, y):
+        """
+        Move the ball by the given x and y amounts.
+        """
+        self.position = self.position[0] + x, self.position[1] + y
+
+    def paint(self, painter: QPainter) -> None:
+        """
+        Paint the ball to an active painter.
+        """
+        painter.drawEllipse(self.position[0] - self.radius,
+                            self.position[1] - self.radius,
+                            self.radius * 2,
+                            self.radius * 2)
+
+
+class PongGame(QLabel):
+    """
+    The Pong Game. Handles the game logic and displays the result.
+    """
+    def __init__(self, parent: Optional[QWidget] = None) -> None:
+        QLabel.__init__(self, parent)
+        self.sideLength = SQUARE_SIZE
+
+        self.ball = Ball()
+        self.leftPaddle = Paddle()
+        self.rightPaddle = Paddle(side=RIGHT)
+
+        self.setFixedSize(self.sideLength, self.sideLength)
+        self.ballDirection = 2, 3
+        self.lostGame = False
+
+        self._timer = QTimer(self)
+        self._timer.setInterval(20)
+        self._timer.timeout.connect(self.updateState)
+        self.isRunning = False
+
+    def updateState(self) -> None:
+        """
+        Move the ball and paddles and check for collisions. Then paint the new
+        state.
+        """
+        if self.ball.leftEdge() == 0 \
+            or self.ball.rightEdge == self.sideLength:
+            self.lostGame = True
+            self._timer.stop()
+        elif self.ball.topEdge() <= 0 \
+            or self.ball.bottomEdge() >= self.sideLength:
+            self.ballDirection = self.ballDirection[0], \
+                -self.ballDirection[1]
+        elif self.leftPaddle.isHit(self.ball):
+            self.ballDirection = abs(self.ballDirection[0]), \
+                self.ballDirection[1]            
+        elif self.rightPaddle.isHit(self.ball):
+            self.ballDirection = -abs(self.ballDirection[0]), \
+                self.ballDirection[1]
+        
+        self.ball.move(*self.ballDirection)
+
+        self.leftPaddle.move()
+        self.rightPaddle.move()
+
+        self.repaint()
+
+    def start(self) -> None:
+        """
+        Start the game or resume it if it is paused.
+        """
+        self._timer.start()
+        self.isRunning = True
+
+    def stop(self) -> None:
+        """
+        Pause the game.
+        """
+        self._timer.stop()
+        self.isRunning = False
+
+    def toggle(self) -> None:
+        """
+        Toggle the game between running and paused.
+        """
+        if self.isRunning:
+            self.stop()
+        else:
+            self.start()
+
+    def paintEvent(self, event: QPaintEvent) -> None:
+        """
+        Paint the game state to the screen.
+        """
+        painter = QPainter(self)
+        painter.setRenderHints(QPainter.Antialiasing, True)
+
+        self.ball.paint(painter)
+        self.leftPaddle.paint(painter)
+        self.rightPaddle.paint(painter)
+        
+        painter.end()
+
+class Window(QWidget):
+    def __init__(self) -> None:
+        """
+        Initialize the window for playing pong.
+        """
+        QWidget.__init__(self)
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+
+        self.game = PongGame()
+        self.layout.addWidget(self.game)
+
+        self.toggleButton = QPushButton("Toggle")
+        self.toggleButton.clicked.connect(self.game.toggle)
+        self.layout.addWidget(self.toggleButton)
+
+        self.setFocus()
+
+    def keyPressEvent(self, e: QKeyEvent) -> None:
+        """
+        Handle key presses.
+        """
+        key = e.key()
+        if key == Qt.Key_Q:
+            self.game.leftPaddle.movingUp = True
+        elif key == Qt.Key_Y:
+            self.game.leftPaddle.movingDown = True
+        elif key == Qt.Key_I:
+            self.game.rightPaddle.movingUp = True
+        elif key == Qt.Key_M:
+            self.game.rightPaddle.movingDown = True
+
+    def keyReleaseEvent(self, e: QKeyEvent) -> None:
+        """
+        Handle key releases.
+        """
+        key = e.key()
+        if key == Qt.Key_Q:
+            self.game.leftPaddle.movingUp = False
+        elif key == Qt.Key_Y:
+            self.game.leftPaddle.movingDown = False
+        elif key == Qt.Key_I:
+            self.game.rightPaddle.movingUp = False
+        elif key == Qt.Key_M:
+            self.game.rightPaddle.movingDown = False
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Window()
+
+    window.show()
+    sys.exit(app.exec())
